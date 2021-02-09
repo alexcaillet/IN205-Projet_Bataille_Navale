741ed11fb9899667590b5c7f1683d7987f227f3b
CAILLET Alexandre <alexcaillet@free.fr>
2021-02-09 12:22:39 +0100

    Début de la création des classes navires

---
+++ bataille-navale/src/main/java/ensta/AbstractShip.java
@@ -0,0 +1,25 @@
+package ensta;
+
+public abstract class AbstractShip{
+    /**
+     * Label qui sera afffiché sur la grille
+     */
+    private char label;
+
+    /**
+     * nom du type de navire
+     */
+    private final String name;
+
+    /**
+     * Taille du bateau
+     */
+    private final int size;
+
+    /**
+     * Orientation
+     */
+    private Orientation orientation;
+
+    
+}
